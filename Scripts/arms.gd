9caf5e88c5ee7b8fb16612112dd0d64cd884cb52
Martin <alpaga.ytb@gmail.com>
2026-03-22 14:44:54 +0100

    The arms are somewhat working

--- Scripts/arms.gd
+++ Scripts/arms.gd
@@ -12,11 +12,12 @@ func _ready() -> void:
 	_on_box_empty()
 
 func _process(delta: float) -> void:
-	var t : Transform3D = skeleton.get_bone_global_pose(
-		skeleton.find_bone("Bone")
-	)
-	in_hand.global_position = skeleton.to_global(t.origin)
-	in_hand.global_basis = t.basis
+	#var t : Transform3D = skeleton.get_bone_global_pose(
+		#skeleton.find_bone("Box")
+	#)
+	#in_hand.global_position = skeleton.to_global(t.origin)
+	#in_hand.global_basis = t.basis
+	in_hand.global_transform = $arms_rigged/Root/rig/Skeleton3D/Box/CerealBoxFlat.global_transform
 	pass
 
 func _on_box_eating():
