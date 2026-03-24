3ae3b2b43544626fc88a7a90f65c95093e2945eb
Alpa <52045495+Alpagateau@users.noreply.github.com>
2026-03-24 09:32:13 +0100

    daycycle again

--- Scripts/end_day_ui.gd
+++ Scripts/end_day_ui.gd
@@ -4,6 +4,9 @@ class_name EndDayUI
 var combinations : Array[TrinketCombination]
 @export_dir var combination_file
 @export var inv : Inventory
+@export var label : PackedScene
+
+var scores : Array
 
 func _ready() -> void:
 	get_all_combination()
@@ -11,10 +14,43 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Esc"):
+		visible = true
 		print(calc_score())
+		animate_screen()
+	if $NextScore.is_stopped() && $NextText.is_stopped() && scores == []:
+		visible = false
 
-func calc_score() -> int:
-	var score : int = 0
+func animate_screen():
+	scores = calc_score()
+	for c in $PanelContainer/VBoxContainer/HBoxContainer/CombNames.get_children():
+		$PanelContainer/VBoxContainer/HBoxContainer/CombNames.remove_child(c)
+		c.queue_free()
+	for c in $PanelContainer/VBoxContainer/HBoxContainer/ComScores.get_children():
+		$PanelContainer/VBoxContainer/HBoxContainer/ComScores.remove_child(c)
+		c.queue_free()
+	draw_next_name()
+
+func draw_next_name():
+	if scores == []:
+		return
+	var new_label : Label = label.instantiate()
+	new_label.text = scores[-1][0]
+	$PanelContainer/VBoxContainer/HBoxContainer/CombNames.add_child(new_label)
+	#scores.pop_back()
+	$NextText.start()
+	$NextScore.start()
+
+func draw_next_score():
+	if scores == []:
+		return
+	var new_label : Label = label.instantiate()
+	new_label.text = str(scores[-1][1])
+	scores.pop_back()
+	$PanelContainer/VBoxContainer/HBoxContainer/ComScores.add_child(new_label)
+
+
+func calc_score() -> Array:
+	var score : Array = []
 	for c in combinations:
 		var valid : bool = true
 		for tv in c.needed:
@@ -28,7 +64,7 @@ func calc_score() -> int:
 				valid = false
 				break
 		if valid:
-			score += c.value
+			score += [[c.name, c.value]]
 	return score
 
 func get_all_combination():
@@ -48,3 +84,8 @@ func get_all_combination():
 				combinations += [c]
 				print("> Loaded Combination ", c.name)
 		file_name = dir.get_next()
+
+
+func _on_day_cycle_day_end() -> void:
+	animate_screen()
+	pass # Replace with function body.
